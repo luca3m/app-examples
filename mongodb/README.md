baef5158a472724f6387139ed3ae683d047552c1
Luca Marturana <lucamarturana@gmail.com>
2015-08-28 11:19:04 +0200

    Import mongodb example

---
+++ mongodb/README.md
@@ -0,0 +1,9 @@
+# mongodb_example
+Example container that executes mongodb queries
+
+to run it type:
+
+```
+docker run -d --name=mongodb_example --link mongodb:mongodb luca3m/mongodb-example
+```
+
---
+++ mongodb/example.rb
@@ -0,0 +1,51 @@
+require 'mongo'
+require 'faker'
+
+include Mongo
+
+client = MongoClient.new(ENV['MONGODB'])
+db= client["mydb"]
+colls = []
+
+100.times do
+	coll = db[Faker::Lorem.word]
+	colls << coll
+	coll.ensure_index({ :name => Mongo::ASCENDING}, { :unique => 1})
+end
+
+
+loop do
+  coll = colls.sample
+  name = Faker::Name.first_name
+  begin
+  	coll.insert( { :name => name,
+                 :surname => Faker::Name.last_name,
+                 :city => Faker::Address.city,
+                 :country_code => Faker::Address.country_code
+                })
+  	coll.insert( { :name => name,
+                 :surname => Faker::Name.last_name,
+                 :city => Faker::Address.city,
+                 :country_code => Faker::Address.country_code
+                })
+  rescue
+  end
+  coll.update({ :name => name}, { "$inc" => { :hits => 1 }})
+  coll.find({ :name => name }).each do |item|
+  	puts item
+  end
+  coll.aggregate( [
+ 	                      { "$match" => {}},
+ 	                      { "$group" => {
+ 	                                  	"_id" => "$country_code",
+ 	                                  	"hits" => { "$sum" => "$hits" }
+ 	                                  	}
+ 	                     }
+ 	             ])
+  coll.map_reduce("function() { emit(this.country_code, this.hits) }",
+                  "function(key,values) { return Array.sum(values) }",  { :out => { :inline => true }, :raw => true});
+  if Random.rand < 0.3
+  	coll.remove({ :name => name})
+  end
+  sleep 0.1
+end
