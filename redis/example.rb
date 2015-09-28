7cc54c355de660baebcf14d6296157d0530c6845
Luca Marturana <lucamarturana@gmail.com>
2015-09-28 12:18:40 +0200

    Configurable speed

--- redis/example.rb
+++ redis/example.rb
@@ -4,9 +4,15 @@ require 'faker'
 
 $redis = Redis.new(host: ENV["REDIS_HOST"])
 
+if ARGV[0]
+  $speed = ARGV[0].to_f
+else
+  $speed = 0.5
+end
+
 loop do
   Random.rand(10).times do
     $redis.rpush Faker::Lorem.word, Faker::Lorem.paragraph
   end
-  sleep 1
+  sleep $speed
 end
