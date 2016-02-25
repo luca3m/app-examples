85d14a22d12ef1e0c32d45585efe9e80d8cf071e
Luca Marturana <lucamarturana@gmail.com>
2016-02-24 16:08:37 -0800

    Random speed

--- redis/example.rb
+++ redis/example.rb
@@ -4,17 +4,11 @@ require 'faker'
 
 $redis = Redis.new(host: ENV["REDIS_HOST"])
 
-if ARGV[0]
-  $speed = ARGV[0].to_f
-else
-  $speed = 0.5
-end
-
 loop do
   k = Faker::Lorem.word
   v = Faker::Lorem.paragraph
   $redis.set k, v
   $redis.get k
   $redis.del k
-  sleep $speed
+  sleep Random.rand
 end
