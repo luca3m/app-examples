6e8d8da5a906c9ca762712ff1040d127c8fe94b0
Luca Marturana <lucamarturana@gmail.com>
2016-08-18 20:35:11 +0200

    Upgrade postgres

--- postgres-example/example.rb
+++ postgres-example/example.rb
@@ -2,6 +2,6 @@ require 'pg'
 $pg = PGconn.connect(ENV['POSTGRES'], 5432, '', '', ENV['POSTGRES_USER'], ENV['POSTGRES_PASSWORD'])
 
 loop do
-  res = conn.exec('SELECT tablename, tableowner from pg_tables')
+  res = $pg.exec('SELECT tablename, tableowner from pg_tables')
   sleep Random.rand
 end
