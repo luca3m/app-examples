3daba750b30e67cf133614b9182e1a4df743c787
Luca Marturana <lucamarturana@gmail.com>
2017-08-15 11:09:55 +0200

    Improve failer example

--- failer/docker-entrypoint.sh
+++ failer/docker-entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-sleep 60
-exit 1
+set -x
+AUTOFAILRATIO=10
+if [ ! -z $1 ]; then
+  AUTOFAILRATIO=$1
+fi
+sleep 5
+touch /healthy
+while true; do
+  if [ -e /fail ]; then
+    exit 1
+  elif [ -e /failcheck ]; then
+    rm /healthy
+  elif [ $(( $RANDOM % $AUTOFAILRATIO )) == 0 ]; then
+    rm /healthy
+  elif [ $(( $RANDOM % $AUTOFAILRATIO )) == 0 ]; then
+    touch /healthy
+  fi
+  sleep 1
+done
