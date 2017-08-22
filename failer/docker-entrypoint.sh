98d733f227e7795b56361ec11dbc0f33ab4c8ba1
Luca Marturana <lucamarturana@gmail.com>
2017-08-22 12:38:56 +0200

    New logic to just fail every 1 minute

--- failer/docker-entrypoint.sh
+++ failer/docker-entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 set -x
-AUTOFAILRATIO=10
-if [ ! -z $1 ]; then
-  AUTOFAILRATIO=$1
-fi
 sleep 5
 touch /healthy
+sleep 60
+rm /healthy
 while true; do
-  if [ -e /fail ]; then
-    exit 1
-  elif [ -e /failcheck ]; then
-    rm /healthy
-  elif [ $(( $RANDOM % $AUTOFAILRATIO )) == 0 ]; then
-    rm /healthy
-  elif [ $(( $RANDOM % $AUTOFAILRATIO )) == 0 ]; then
-    touch /healthy
-  fi
-  sleep 1
+  sleep 10
 done
