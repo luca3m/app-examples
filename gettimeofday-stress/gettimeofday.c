17786c1585075bac1a4bed4f75b2c3f1705ee370
Luca Marturana <lucamarturana@gmail.com>
2017-09-29 12:48:40 +0200

    update gettimeofday to always use syscall

--- gettimeofday-stress/gettimeofday.c
+++ gettimeofday-stress/gettimeofday.c
@@ -1,10 +1,13 @@
 #include <sys/time.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 int main()
 {
   for(;;)
   {
     struct timeval tv;
-    gettimeofday(&tv, 0);
+    // gettimeofday(&tv, 0);
+    syscall(SYS_gettimeofday, &tv, 0);
   }
 }
