b5461d22506c076bb3894ef9066dea0113149ee5
Luca Marturana <luca@sysdig.com>
2018-01-30 16:01:45 -0800

    Add hello url to http server

--- http-server/main.go
+++ http-server/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"net/http"
 	"log"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
-
+	http.HandleFunc("/random_wait", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration((rand.Int() % 10)) * time.Millisecond)
+	})
+	http.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/hello/"):]
+		w.Write([]byte("hello " + name))
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
